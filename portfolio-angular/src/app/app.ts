65561a633712cac96d6461145c0f2c5a0703ff04
Gabriela Bomfati Garcia <20241ctb0100051@estudantes.ifpr.edu.br>
2026-06-13 13:49:06 +0000

    Aula16: Cria componentes, navegacao com Material e rotas do Portfolio

--- portfolio-angular/src/app/app.ts
+++ portfolio-angular/src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('Meu Portfólio');
-}
+export class App {}
